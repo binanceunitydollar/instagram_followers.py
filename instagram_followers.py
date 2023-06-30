28b1316e42562bc1619d809cecd7b86746a13782
binanceunitydollar <127662801+binanceunitydollar@users.noreply.github.com>
2023-06-30 09:27:53 +0000

    Update instagram_followers.py

--- instagram_followers.py
+++ instagram_followers.py
@@ -9,9 +9,13 @@ password = input("Enter your Instagram password: ")
 loader.login(username, password)
 
 # Retrieve the profile of the target account
-target_username = "target_username"
-profile = instaloader.Profile.from_username(loader.context, dpz_cartoon_)
+target_username = input("Enter the target account username: ")
+profile = instaloader.Profile.from_username(loader.context, target_username)
+
+# Get the followers of the target account
+followers = profile.get_followers()
 
 # Rest of the code...
+
 
 
