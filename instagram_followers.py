8dbd40590315f78cd74cdc9f4fbb154ebbd7eb1c
binanceunitydollar <127662801+binanceunitydollar@users.noreply.github.com>
2023-06-30 09:04:48 +0000

    Update instagram_followers.py

--- instagram_followers.py
+++ instagram_followers.py
@@ -1,18 +1,17 @@
 import instaloader
+
 # Create an instance of Instaloader
 loader = instaloader.Instaloader()
 
-# Login to an Instagram account (optional)
-loader.login('herrlybag', 'saadsaad')
+# Login to your Instagram account
+username = input("Enter your Instagram username: ")
+password = input("Enter your Instagram password: ")
+loader.login(username, password)
 
 # Retrieve the profile of the target account
-profile = instaloader.Profile.from_username(loader.context, 'girls_anime_dpz')
+target_username = "target_username"
+profile = instaloader.Profile.from_username(loader.context, target_username)
 
-# Get the followers of the target account
-followers = profile.get_followers()
-
-# Iterate over the followers and print their usernames
-for follower in followers:
-    print(follower.username)
+# Rest of the code...
 
 
